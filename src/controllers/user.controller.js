72de4a48c6f58506e1096fcb228b27ef3ace3cd2
hannibal1631 <mukherjeerupak860@gmail.com>
2025-10-27 11:16:47 +0530

    upload avatar to cloudinary

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -28,6 +29,14 @@ const registerUser = asyncHandler(async (req, res) => {
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar is required");
+  }
+
+  // upload images and avatar to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if (!avatar) {
     throw new ApiError(400, "avatar is required")
   }
 
