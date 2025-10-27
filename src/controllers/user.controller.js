3e70bed9e20e51b8606f6e21d49fa743ad88bc66
hannibal1631 <mukherjeerupak860@gmail.com>
2025-10-27 11:23:49 +0530

    create user object

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,13 +33,22 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // upload images and avatar to cloudinary
-  const avatar = await uploadOnCloudinary(avatarLocalPath)
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
-    throw new ApiError(400, "avatar is required")
+    throw new ApiError(400, "avatar is required");
   }
 
+  // create user object and create entry in db
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
       type: String, // cloudinary url
       required: true,
     },
-    coverimage: {
+    coverImage: {
       type: String,
     },
     watchHistory: [
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
@@ -62,17 +62,18 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
-  return jwt.sign({
-    _id: this._id,
-    email: this.email,
-    username: this.username,
-    fullname: this.fullname,
-  },
-  process.env.ACCESS_TOKEN_SECRET,
-  {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-  }
-);
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
 };
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
