a980b5b3e401375ae1b509c166c18b58bbec599e
hannibal1631 <mukherjeerupak860@gmail.com>
2025-10-27 11:01:52 +0530

    add user detail validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
   const { fullname, email, username, password } = req.body;
   console.log("email: ", email);
 
-  
-
+  // check validation
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 });
 
 export { registerUser };
