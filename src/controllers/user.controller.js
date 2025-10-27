00cd4893c061a2e0997dd8d6bd88c852fad20f30
hannibal1631 <mukherjeerupak860@gmail.com>
2025-10-27 11:32:14 +0530

    send user response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -53,11 +54,16 @@ const registerUser = asyncHandler(async (req, res) => {
   // check if user is created successfully
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
-  )
+  );
 
   if (!createdUser) {
-    throw new ApiError(500, "something went wrong while registering the user")
+    throw new ApiError(500, "something went wrong while registering the user");
   }
+
+  // return the response to user
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+  )
 
 });
 
