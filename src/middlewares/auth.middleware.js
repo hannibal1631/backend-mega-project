0afe22d61e032db6b2af4387ffdbc5bcea85a322
hannibal1631 <mukherjeerupak860@gmail.com>
2025-10-31 19:54:21 +0530

    add token functionality

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,7 @@
+import { User } from "../models/user.model";
 import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
 
 export const varifyJWT = asyncHandler(async (req, res, next) => {
   const token =
@@ -9,4 +11,16 @@ export const varifyJWT = asyncHandler(async (req, res, next) => {
   if (!token) {
     throw new ApiError(401, "Unauthorized Request");
   }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+  if (!user) {
+    throw new ApiError(401, "Invalid access token")
+  }
+
+  req.user = user;
+  next()
+
 });
