fbdddac11498fad0bac0305fb6879587187aa1dc
hannibal1631 <mukherjeerupak860@gmail.com>
2025-11-04 23:54:05 +0530

    add cover image update function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -295,6 +295,34 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   ).select("-password");
 });
 
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "Cover image file is missing");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new ApiError(400, "Error while uploading cover image");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover image updated successfully"));
+});
+
 export {
   registerUser,
   loginUser,
@@ -304,4 +332,5 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
+  updateUserCoverImage,
 };
